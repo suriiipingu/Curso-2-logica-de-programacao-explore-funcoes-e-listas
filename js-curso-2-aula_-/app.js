03667c396085e8fd766b2864b6bc58c1c1d4efe8
Clara Sanson <btsherwatching@gmail.com>
2024-06-25 21:45:03 -0300

    aula 1

--- js-curso-2-aula_-/app.js
+++ js-curso-2-aula_-/app.js
@@ -3,3 +3,9 @@ titulo.innerHTML = 'Jogo do número secreto';
 
 let paragrafo = document.querySelector('p');
 paragrafo.innerHTML = 'Escolha um número entre 1 e 10';
+
+//função = trecho do código que exerce uma função
+function verificarChute(){
+    //console.log('O botão foi clicado');
+    
+}
